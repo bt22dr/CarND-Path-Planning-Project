c600bc8844c76c6a2734de3eb0418af5f65d9149
bt22dr <bt22dr@gmail.com>
2019-03-11 13:30:19 +0900

    이동 경로에 spline 보간 적용

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "Eigen-3.3/Eigen/QR"
 #include "helpers.h"
 #include "json.hpp"
+#include "spline.h"
 
 // for convenience
 using nlohmann::json;
@@ -97,15 +98,93 @@ int main() {
            * TODO: define a path made up of (x,y) points that the car will visit
            *   sequentially every .02 seconds
            */
-          double dist_inc = 0.5;
-          for (int i = 0; i < 50; ++i) {
-            double next_s = car_s + (i+1)*dist_inc;
-            double next_d = car_d;
-            vector<double> next_xy = getXY(next_s, next_d,
-                map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          int lane = 1;
+          double ref_vel = 49.5; 
 
-            next_x_vals.push_back(next_xy[0]);
-            next_y_vals.push_back(next_xy[1]);
+          int prev_size = previous_path_x.size();
+
+          vector<double> ptsx;
+          vector<double> ptsy;
+
+          double ref_x = car_x;
+          double ref_y = car_y;
+          double ref_yaw = deg2rad(car_yaw);
+
+          if(prev_size < 2) { // prev size가 거의 비었을 때 이전 위치(추정값)이랑 현재 위치를 ptsx, ptsy에 넣어준다. 
+            double prev_car_x = car_x - cos(car_yaw);
+            double prev_car_y = car_y - sin(car_yaw);
+
+            ptsx.push_back(prev_car_x);
+            ptsx.push_back(car_x);
+            ptsy.push_back(prev_car_y);
+            ptsy.push_back(car_y);
+          } else { // 이게 일반적인 상황에서의 로직
+            ref_x = previous_path_x[prev_size-1];
+            ref_y = previous_path_y[prev_size-1];
+
+            double ref_x_prev = previous_path_x[prev_size-2];
+            double ref_y_prev = previous_path_y[prev_size-2];
+            ref_yaw = atan2(ref_y-ref_y_prev, ref_x-ref_x_prev);
+
+            ptsx.push_back(ref_x_prev);
+            ptsx.push_back(ref_x);
+            ptsy.push_back(ref_y_prev);
+            ptsy.push_back(ref_y);
+          }
+
+          // starting reference 앞쪽으로 30m 마다 pts를 추가함.
+          vector<double> next_wp0 = getXY(car_s+30, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> next_wp1 = getXY(car_s+60, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> next_wp2 = getXY(car_s+90, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+          ptsx.push_back(next_wp0[0]);
+          ptsx.push_back(next_wp1[0]);
+          ptsx.push_back(next_wp2[0]);
+          ptsy.push_back(next_wp0[1]);
+          ptsy.push_back(next_wp1[1]);
+          ptsy.push_back(next_wp2[1]);
+
+          // spline 라이브러리에서 사용하기 위해 이 pts들을 모두 local 좌표계로 바꿔준다. 
+          for (int i = 0; i < ptsx.size(); i++) {
+            double shift_x = ptsx[i]-ref_x;
+            double shift_y = ptsy[i]-ref_y;
+
+            ptsx[i] = (shift_x*cos(0-ref_yaw) - shift_y*sin(0-ref_yaw));
+            ptsy[i] = (shift_x*sin(0-ref_yaw) + shift_y*cos(0-ref_yaw));
+          }
+
+          tk::spline s;
+          s.set_points(ptsx, ptsy);
+
+          for(int i = 0; i < previous_path_x.size(); i++) {
+            next_x_vals.push_back(previous_path_x[i]);
+            next_y_vals.push_back(previous_path_y[i]);
+          }
+
+          double target_x = 30.0;
+          double target_y = s(target_x);
+          double target_dist = sqrt(target_x*target_x + target_y*target_y);
+
+          double x_add_on = 0;
+
+          // 50개 채우기 위해 추가해야하는 점 개수만큼 
+          for(int i = 1; i <= 50-previous_path_x.size(); i++) {
+            double N = (target_dist/(.02*ref_vel/2.24)); // 2.24는 meter랑 mph 변환하는 상수 
+            double x_point = x_add_on+(target_x)/N;
+            double y_point = s(x_point);
+            x_add_on = x_point;
+
+            // 아래 코드는 local 좌표계를 다시 xy 좌표계로 바꿔주는 코드 
+            double x_ref = x_point;
+            double y_ref = y_point;
+            x_point = (x_ref*cos(ref_yaw) - y_ref*sin(ref_yaw));
+            y_point = (x_ref*sin(ref_yaw) + y_ref*cos(ref_yaw));
+            x_point += ref_x;
+            y_point += ref_y;
+
+            // 최종적으로 결과 벡터에 집어넣기
+            next_x_vals.push_back(x_point);
+            next_y_vals.push_back(y_point);
           }
 
           msgJson["next_x"] = next_x_vals;
